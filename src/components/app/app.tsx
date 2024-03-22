d1ad77282c613d2e73df72ed84420175b63ef932
Maria <mariamdeeva@yandex.ru>
2024-03-22 12:21:21 +0300

    Шаг 1. роутинг

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,13 +1,89 @@
-import { ConstructorPage } from '@pages';
+import {
+  ConstructorPage,
+  Feed,
+  ForgotPassword,
+  Login,
+  NotFound404,
+  Profile,
+  ProfileOrders,
+  Register,
+  ResetPassword
+} from '@pages';
 import '../../index.css';
 import styles from './app.module.css';
-
-import { AppHeader } from '@components';
+import { AppHeader, IngredientDetails, Modal, OrderInfo } from '@components';
+import { Route, Routes, useNavigate } from 'react-router-dom';
+import { ProtectedRoute } from '../protected-route/ProtectedRoute';
 
 const App = () => (
   <div className={styles.app}>
     <AppHeader />
-    <ConstructorPage />
+    <Routes>
+      <Route path='/' element={<ConstructorPage />} />
+      <Route path='/feed' element={<Feed />} />
+      <Route path='/login'
+        element={
+          <ProtectedRoute>
+            <Login />
+          </ProtectedRoute>} />
+      <Route path='/register'
+        element={
+          <ProtectedRoute>
+            <Register />
+          </ProtectedRoute>} />
+      <Route path='/forgot-password'
+        element={
+          <ProtectedRoute>
+            <ForgotPassword />
+          </ProtectedRoute>} />
+      <Route path='/reset-password'
+        element={
+          <ProtectedRoute>
+            <ResetPassword />
+          </ProtectedRoute>} />
+      <Route path='/profile'
+        element={
+          <ProtectedRoute>
+            <Profile />
+          </ProtectedRoute>} />
+      <Route path='/profile/orders'
+        element={
+          <ProtectedRoute>
+            <ProfileOrders />
+          </ProtectedRoute>} />
+      <Route path='*' element={<NotFound404 />} />
+      <Route path='/feed/:number'
+        element={
+          <Modal
+            title={''}
+            onClose={function (): void {
+              throw new Error('OrderInfo');
+            }}
+          >
+            <OrderInfo />
+          </Modal>} />
+
+      <Route path='/ingredients/:id'
+        element={
+          <Modal
+            title={''}
+            onClose={function (): void {
+              throw new Error('IngredientDetails');
+            }}
+          >
+            <IngredientDetails />
+          </Modal>} />
+      <Route path='/profile/orders/:number'
+        element={
+          <Modal
+            title={''}
+            onClose={function (): void {
+              throw new Error('OrderInfo: number');
+            }}
+          >
+            <OrderInfo />
+          </Modal>} />
+    </Routes>
   </div>
 );
 
